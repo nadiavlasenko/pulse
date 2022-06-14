df6e81475bb3c4b432bfd5c6df0e300469a7b158
Nadia <vlasenkonadia6@gmail.com>
2022-06-14 23:41:34 +0100

    add icon

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,25 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "#source"
+            baseDir: "src"
         }
     });
 
-    gulp.watch("index.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
-    return gulp.src("#source/sass/**/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("#source/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));    
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
